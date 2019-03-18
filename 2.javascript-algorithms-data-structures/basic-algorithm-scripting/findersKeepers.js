cba1bb5abd9968bc49c920eef75e03b8eae0e36e
htrul18 <htrul18@gmail.com>
2019-03-18 22:41:43 +0100

    Add findersKeepers.js

---
+++ 2.javascript-algorithms-data-structures/basic-algorithm-scripting/findersKeepers.js
@@ -0,0 +1,15 @@
+function findElement(arr, func) {
+  let num = 0;
+  for (let i=0;i<arr.length;i++){
+    num = arr[i];
+    if (func(num)){
+      return num;
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+findElement([1, 3, 5, 8, 9, 10], num => num % 2 === 0);
+findElement([1, 3, 5, 9], num => num % 2 === 0);
+
